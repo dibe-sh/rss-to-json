65154aadc85f224c3da34b0be14aca0d6ac62b85
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2024-11-29 23:26:07 +0545

    remove explicit binding to all network interfaces on application startup

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   console.log(`Application is running on: ${port}`);
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
 }
 bootstrap();
